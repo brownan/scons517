73a36b627674712a80c70a1906219fdd60530c85
Andrew Brown <andrew@brownan.org>
2023-02-07 12:41:18 -0500

    Changed get_pure_tag to use "py" instead of "cp" for interpreter

--- scons517/__init__.py
+++ scons517/__init__.py
@@ -12,14 +12,31 @@ def tool(env: Environment):
 
 def get_binary_tag():
     """Gets the most specific binary tag for the current machine"""
+    # This looks through sys_tags() for the first tag that doesn't use the manylinux platform.
+    # Why do we skip the manylinux platform?
+    #
+    # sys_tags() lists tags compatible with the current system in order. since
+    # the generic linux_x86_64 platform is not a precise enough tag to guarantee compatibility,
+    # the packaging library considers "manylinux" to be a higher priority for installation
+    # candidates.
+    #
+    # However, we are not choosing an installation candidate, we want to find a tag for the
+    # distribution being built, so our priorities are a bit different. Instead, it makes sense
+    # for linux builds to use the imprecise linux_x86_64 (or similar) platform, since
+    # we cannot programmatically guarantee platform compatibility with the manylinux spec.
+    # In the future, maybe there is some way to incorporate functionality from the "auditwheel"
+    # tool to automatically determine a more precise linux platform.
     return str(
         next(tag for tag in packaging.tags.sys_tags() if "manylinux" not in tag.platform)
     )
 
 
 def get_pure_tag():
-    """Gets the tag for a pure-python wheel with no platform specific aspects"""
+    """Gets the tag for a pure-python wheel with no platform specific aspects compatible
+    with the current python version and up
+
+    """
     interp_tag = (
-        f"{packaging.tags.interpreter_name()}{packaging.tags.interpreter_version()}"
+        f"py{packaging.tags.interpreter_version()}"
     )
     return f"{interp_tag}-none-any"
